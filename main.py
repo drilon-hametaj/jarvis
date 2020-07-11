da888076c91cb26bab114793b41ce1bc8ee62d77
drilon-hametaj <drilonhametaj@outlook.it>
2020-07-11 19:34:23 +0200

    wishMe function

--- main.py
+++ main.py
@@ -6,4 +6,34 @@ import webbrowser
 import os
 import smtplib
 
+nome = "Drilon"
+print("Initializing Poncho")
+
+engine = pyttsx3.init("sapi5")
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
+
+# Speak function will pronounce the string which is passed to it
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour >= 0 and hour < 12:
+        speak("Buongiorno "+nome)
+
+    elif hour >= 12 and hour<18:
+        speak("Buon pomeriggio "+nome)
+
+    else:
+        speak("Buonasera "+nome)
+
+    speak("Come posso aiutarti?")
+
+
+
+speak("Ciao "+nome+", sono Poncio")
 
